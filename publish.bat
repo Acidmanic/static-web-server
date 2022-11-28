1efb0ea8b8c4b5a16af41307ee248451b36c4e2b
Mani <acidmanic.moayedi@gmail.com>
2022-11-29 01:46:56 +0330

    Fix publish script for windows

--- publish.bat
+++ publish.bat
@@ -6,8 +6,9 @@ set RUNTIME=win10-x64
 set OUTPUT=publish\%RUNTIME%
 
 
-dotnet publish --output %OUTPUT% --self-contained true --runtime %RUNTIME% --framework netcoreapp3.1 -p:PublishReadyToRun=true -p:PublishSingleFile=false \
-&& cd %OUTPUT% && 7z a -tzip ..\%RUNTIME%.zip . 
-cd .. \
-&& rmdir %OUTPUT% /s /q  
+dotnet publish --output %OUTPUT% --self-contained true --runtime %RUNTIME% --framework netcoreapp3.1 -p:PublishReadyToRun=true -p:PublishSingleFile=false
+cd %OUTPUT% && 7z a -tzip ..\%RUNTIME%.zip . 
+cd ..
+cd ..
+rmdir %OUTPUT% /s /q  
 
